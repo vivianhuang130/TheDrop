fcd2fe9e980d32f00aac65e6d897aaff8b6c9d43
Lorin Shamos <lshamos@gmail.com>
2017-07-16 21:25:50 -0700

    node setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const
+  dotenv = require('dotenv').load(),
+  express = require('express'),
+  app = express(),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  port = 3000,
+
+mongoose.connect('mongodb://the-drop', (err) => {
+  console.log(err || "Connected to MongoDB.")
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+
+
+app.listen(port, function(err) {
+  console.log(err || `Server is running on ${port}`)
+})
