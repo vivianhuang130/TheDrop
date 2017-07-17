b0d3bed8c1dd5edff01cc9ec5a242f17aa0d73bc
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2017-07-17 13:34:13 -0700

    working passport and weather API

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const
   dotenv = require('dotenv').load(),
   express = require('express'),
   app = express(),
-  logger = require('morgan'),
+  morgan = require('morgan'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
+  cookieParser = require('cookie-parser'),
+  passport = require('passport'),
+  session= require ('express-session'),
+  flash = require('flash'),
   port = 3000,
+  User = require('./models/User.js'),
+  Comment = require('./models/Comment.js'),
+  SurfLocation = require('./models/SurfLocation.js'),
   weatherController = require('./controllers/weather.js')
 
 mongoose.connect('mongodb://localhost/the-drop', (err) => {
@@ -14,9 +21,17 @@ mongoose.connect('mongodb://localhost/the-drop', (err) => {
 
 app.set('view engine', "ejs")
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'))
+app.use(session({ secret: 'THE-DROP-PROJECT-3' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+require('./config/passport')(passport);
 
 app.get('/', weatherController.index)
 app.get('/search/:searchTerm', weatherController.search)
