6fd36a66ea954c41853469f2d5ce1ebaabda3532
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2017-07-17 13:08:28 -0700

    working weather api again

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ mongoose.connect('mongodb://localhost/the-drop', (err) => {
   console.log(err || "Connected to MongoDB.")
 })
 
+app.set('view engine', "ejs")
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', weatherController.index)
 app.get('/search/:searchTerm', weatherController.search)
