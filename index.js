08607c173e8a5db15938df75e6dc869a537c6df8
Vivian Huang <vivianhuang0130@gmail.com>
2017-07-18 16:53:29 -0700

    git pull

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const
   MongoDBStore = require('connect-mongodb-session')(session),
   express = require('express'),
   app = express(),
+  fs = require("fs"),
   ejs = require('ejs'),
+  multer = require("multer"),
+  upload = multer({dest: "./uploads"}),
   ejsLayouts = require('express-ejs-layouts'),
   morgan = require('morgan'),
   bodyParser = require('body-parser'),
@@ -32,6 +35,60 @@ const
 mongoose.connect(mongoConnectionString, (err) => {
 	console.log(err || "Connected to MongoDB (passport-authentication)")
 })
+var conn = mongoose.connection;
+
+var gfs;
+var Grid = require("gridfs-stream");
+Grid.mongo = mongoose.mongo;
+
+conn.once("open", function(){
+  gfs = Grid(conn.db);
+  app.get("/", function(req,res){
+    //renders a multipart/form-data form
+    res.render("home");
+  });
+
+  //second parameter is multer middleware.
+  app.post("/", upload.single("avatar"), function(req, res, next){
+    //create a gridfs-stream into which we pipe multer's temporary file saved in uploads. After which we delete multer's temp file.
+    var writestream = gfs.createWriteStream({
+      filename: req.file.originalname
+    });
+    //
+    // //pipe multer's temp file /uploads/filename into the stream we created above. On end deletes the temporary file.
+    fs.createReadStream("./uploads/" + req.file.filename)
+      .on("end", function(){fs.unlink("./uploads/"+ req.file.filename, function(err){res.send("success")})})
+        .on("err", function(){res.send("Error uploading image")})
+          .pipe(writestream);
+  });
+
+  // sends the image we saved by filename.
+  app.get("/:filename", function(req, res){
+      var readstream = gfs.createReadStream({filename: req.params.filename});
+      readstream.on("error", function(err){
+        res.send("No image found with that title");
+      });
+      readstream.pipe(res);
+  });
+
+  //delete the image
+  app.get("/delete/:filename", function(req, res){
+    gfs.exist({filename: req.params.filename}, function(err, found){
+      if(err) return res.send("Error occured");
+      if(found){
+        gfs.remove({filename: req.params.filename}, function(err){
+          if(err) return res.send("Error occured");
+          res.send("Image deleted!");
+        });
+      } else{
+        res.send("No image found with that title");
+      }
+    });
+  });
+});
+
+
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -75,15 +132,6 @@ app.use((req, res, next) => {
 //ejs config
 app.set('view engine', "ejs")
 // app.use(ejsLayouts)
-
-
-
-
-
-
-
-
-
 
 app.use(flash());
 
