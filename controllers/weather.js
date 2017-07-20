e394a38466817b45b80078ddc906ff2307f456a9
Lorin Shamos <lshamos@gmail.com>
2017-07-20 11:59:46 -0700

    fixed heroku package.json

--- controllers/weather.js
+++ controllers/weather.js
@@ -11,7 +11,6 @@ module.exports = {
     console.log(req.params)
     var spotId = req.params.searchTerm
     var surfApiUrl = 'http://api.surfline.com/v1/forecasts'
-    //<spot_id>?resources=&days=&getAllSpots=&units=&usenearshore=&interpolate=&showOptimal=&callback=
     var surfRequestUrl = `${surfApiUrl}/${spotId}?surf,wind,tide,weather=&2=&false=&e=&true=&true=&true=&callback=`
 
     console.log(surfRequestUrl)
